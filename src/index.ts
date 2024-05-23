c752829010f0e2a334acb1d6f68084bbc90289ed
bellokin <121698550+bellokin@users.noreply.github.com>
2024-05-23 17:22:22 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { Server } from "./server";
 require('dotenv').config();
 
-// Parse the PORT as a number
+const port = parseInt(process.env.PORT || '5000', 10);  
+const host = process.env.HOST || "0.0.0.0"; // Use HOST environment variable or default to '0.0.0.0'
 
 const server = new Server();
 
-server.listen(port => {
-  console.log(`Server is listening cxcd on http://localhost:${port}`);
+server.listen(port, host, () => {
+  console.log(`Server is listening on http://${host}:${port}`);
 });
