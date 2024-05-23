82973e58ddb37d021d28492af6c3c80e3d477f87
Ibukun Bello <BelloI@Coronationmb.com>
2024-05-23 14:53:01 +0100

    Kachasi

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Server } from "./server";
+require('dotenv').config();
 
 const server = new Server();
 
-server.listen(port => {
-  console.log(`Server is listening on `);
-});
+// Get port from environment variable or use a default value
+const port: number = parseInt(process.env.PORT || '4000', 10);
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
