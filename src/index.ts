41841606073a128ff4686f7b21a80d81e1be9c27
bellokin <121698550+bellokin@users.noreply.github.com>
2024-05-23 17:16:23 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ require('dotenv').config();
 const server = new Server();
 
 server.listen(port => {
-  console.log(`Server is listening on http://localhost:${port}`);
+  console.log(`Server is listening cxcd on http://localhost:${port}`);
 });
