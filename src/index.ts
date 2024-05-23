48b0c466b5172160b1b689341b0b63a1d4f0d62a
Ibukun Bello <BelloI@Coronationmb.com>
2024-05-23 15:55:27 +0100

    fsd

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Server } from "./server";
 require('dotenv').config();
 
+// Parse the PORT as a number
+const port = parseInt(process.env.PORT || '5000', 10);  
+const host = process.env.HOST || "0.0.0.0"; // Use HOST environment variable or default to '0.0.0.0'
+
 const server = new Server();
-const port = parseInt(process.env.PORT || '5000', 10); // Parse the PORT as a number
-const host = process.env.HOST || '0.0.0.0'; // Use HOST environment variable or default to '0.0.0.0'
 
 server.listen(port, host, () => {
-  console.log(`Server is listening on http://${host}:${port}`);
+  console.log(`Server is listening on http://${port}`);
 });
