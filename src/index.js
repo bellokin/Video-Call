04c66a65616786dfcb3c6c0c654d4711e0beeca5
Ibukun Bello <BelloI@Coronationmb.com>
2024-05-23 16:16:55 +0100

    dfsh

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ const port = parseInt(process.env.PORT || '4000', 10);
 const host = process.env.HOST || "0.0.0.0"; // Use HOST environment variable or default to '0.0.0.0'
 const server = new server_1.Server();
 server.listen(port, host, () => {
-    console.log(`Server is listeningxa on http://${port}`);
+    console.log(`Server is listeningxijla on http://${port}`);
 });
